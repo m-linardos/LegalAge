7997df64abdab1a229403104c9fadb4bbfbad81a
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-28 12:43:55 -0400

    Added pseudocode

--- LegalAge/Program.cs
+++ LegalAge/Program.cs
@@ -6,7 +6,28 @@ namespace LegalAge
     {
         static void Main(string[] args)
         {
+            // can they vote or drink?  How mnay years old?
+            // search for ways to subrract 2 dates how to calculate age from date. (no, that's getting too much into the weeds)
+            // var age = today.Year - birthDate.Year;
+
+
+
+
+            //First Option:
            
-        }
+            //DateTime(2000, 8, 15);
+            //if (birthDate > today.AddYears(-LegalAge))
+            //{
+            //    LegalAge--;
+            //}
+
+
+            // Second Option:
+            //DateTime(2000, 8, 15);
+            //int age = someComplexAlgorithmFromTheWeb();
+            //if (bithDate.AddYears(21) >= DateTime.Now)
+            //{ }
+
+            //WHAT ABOUT LEAP YEAR
     }
 }
